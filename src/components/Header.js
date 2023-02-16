8d95a262869fe35dc1a158a8208b63d1c1c81c64
Divy <divy.jain@bombaysoftwares.com>
2023-02-16 18:23:14 +0530

    -added image & search box for top nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,17 +5,27 @@ import React from 'react'
    return (
     <header>
         {/* Top Nav */}
-        <div>
-            <div>
+        <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+
+            {/* Image */}
+            <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                 <Image
                     src="https://links.papareact.com/f90"
                     width={150}
                     height={40}
+                    className="cursor-pointer"
                 />
+            </div>
+
+            {/* Search */}
+            <div>
+                <input type="text"/>
             </div>
         </div>
         {/* Bottom Nav */}
-        <div>Bottom</div>
+        <div>
+
+        </div>
     </header>
    )
  }
