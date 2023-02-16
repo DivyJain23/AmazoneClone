8d2a70b42b966574b12efdbb520b442cbb753fc2
Divy <divy.jain@bombaysoftwares.com>
2023-02-16 19:06:08 +0530

    added search box and heroicons and alligned the items

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,40 @@
- import Image from 'next/image'
+import Image from 'next/image'
 import React from 'react'
- 
- function Header() {
-   return (
-    <header>
-        {/* Top Nav */}
-        <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+import {
+    MenuIcon,
+    SearchIcon,
+    ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
-            {/* Image */}
-            <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-                <Image
-                    src="https://links.papareact.com/f90"
-                    width={150}
-                    height={40}
-                    className="cursor-pointer"
-                />
+function Header() {
+    return (
+        <header>
+            {/* Top Nav */}
+            <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+                {/* Image */}
+                <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+                    <Image
+                        src="https://links.papareact.com/f90"
+                        width={150}
+                        height={40}
+                        className="cursor-pointer h-12"
+                    />
+                </div>
+
+                {/* Search */}
+                <div className='hidden sm:flex items-center h-12 rounded-md flex-grow  cursor-pointer bg-yellow-400 hover:bg-yellow-500 mx-3'>
+                    <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4'
+                        type="text"
+                    />
+                    <SearchIcon className='h-12 p-4'/>
+                </div>
             </div>
-
-            {/* Search */}
+            {/* Bottom Nav */}
             <div>
-                <input type="text"/>
-            </div>
-        </div>
-        {/* Bottom Nav */}
-        <div>
 
-        </div>
-    </header>
-   )
- }
- 
- export default Header
+            </div>
+        </header>
+    )
+}
+
+export default Header
